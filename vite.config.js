b0c70d5a2656dc50da58a8230a3a91662e619a60
pedrodomingosdev <pedrodomingos.profissional@gmail.com>
2026-06-12 19:16:32 -0300

    Alterações para fazer o Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-refresh' 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pedrodomingosdev.github.io/6a0/', 
 })
